fea00377c2f18fb7bdb2465de88ca83bf7702cc5
mobideck <mathieu@mobideck.net>
2016-06-29 18:59:54 +0200

    update api

--- appdeck-ios-iap/Classes/IAPAppDeckPlugin.m
+++ appdeck-ios-iap/Classes/IAPAppDeckPlugin.m
@@ -57,7 +57,7 @@
         
         NSLog(@"Result: %@", result);
         
-        [call sendCallbackWithResult:@[result]];
+        [call sendCallbackWithResult:@[validProducts]];
         
     } failure:^(NSError *error) {
         NSLog(@"Error: %@", error);
